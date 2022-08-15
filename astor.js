7bd94e7acc5de4c99685ffd648a68cdeae8b3acd
unknown <sidlsh7569@gmail.com>
2022-08-14 18:31:15 -0700

    Comp ass

---
+++ astor.js
@@ -0,0 +1,4 @@
+document.querySelector("#custom-love-modal").addEventListener("click",function(){
+    var lovecal = Math.floor(Math.random()*100)+1;
+    alert("your love percentage is: "+ lovecal + "%");
+})
